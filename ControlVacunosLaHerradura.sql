12e45c2122921ffb04453409ba6e3d5c328eb31a
facugarcilazo <97338977+facugarcilazo@users.noreply.github.com>
2025-05-18 18:14:17 -0300

    Add files via upload

---
+++ ControlVacunosLaHerradura.sql
@@ -0,0 +1,3 @@
+INSERT INTO AplicacionVacuna VALUES (NULL, 'AR1234', 1, '2024-01-15', '2025-01-15');
+select * from AplicacionVacuna;
+INSERT INTO PesoRegistro VALUES (NULL, 'AR1234', '2024-06-01', 455.00); 
